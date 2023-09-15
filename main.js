3d79524c2c1671645184145464ced2cf9c43a95f
German Novikov <difenz1k@gmail.com>
2023-09-15 21:41:23 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function displayPhrase()
+{
+    document.getElementById("demo").innerHTML = 'Успешно!';
+    document.getElementById("demo").style.backgroundColor = "#4BB543";
+    document.getElementById("demo").style.color = "#fff"
+}
